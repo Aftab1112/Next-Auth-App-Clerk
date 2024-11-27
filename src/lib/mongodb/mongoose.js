0872b8c52340f2aad19996ea49cb66f9a71a0e1a
Aftab1112 <you@example.com>
2024-11-27 17:52:08 +0530

    Connected clerk with mongodb using webhook

---
+++ src/lib/mongodb/mongoose.js
@@ -0,0 +1,22 @@
+import mongoose from "mongoose";
+
+let initialized = false;
+
+export const connect = async () => {
+  mongoose.set("strictQuery", true);
+
+  if (initialized) {
+    console.log("Mongodb already connected");
+    return;
+  }
+
+  try {
+    await mongoose.connect(process.env.MONGODB_URI, {
+      dbName: "Next Auth App",
+    });
+    console.log("Mongodb connected");
+    initialized = true;
+  } catch (error) {
+    console.error("Mongodb connection error", error);
+  }
+};
